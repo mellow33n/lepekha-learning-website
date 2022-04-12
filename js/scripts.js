7167d1d2838196ab88f2e637919a4f694635014b
mellow33n <kramzeee@gmail.com>
2022-04-12 20:04:13 +0300

    fix error

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ function displayOpa() {
     if(pass == 'qq123') {
         alert('Ладно, заходи');
         document.getElementById("password-page").removeAttribute("style");
-    } else if (pass == 'pidoras' || pass == 'пидорас' || pass == 'лох' || pass == 'loh' || pass == 'lox' || pass == 'pidor' || pass == 'пидор') {
+    } else if (pass == 'pidoras' || pass == 'пидорас' || pass == 'лох' || pass == 'loh') {
         alert('Кодовое слово, пожалуйста. Ваше имя мне не интересно');
         location.reload();
     }
