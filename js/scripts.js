9b2d605fff9652a13582a3a758373913fb6f1acb
mellow33n <kramzeee@gmail.com>
2022-04-13 00:06:54 +0300

    lessons sect done

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 function displayOpa() { 
-    var pass = prompt("Кодовое слово"); 
+    document.getElementById("password-page").removeAttribute("style");
+   var pass = prompt("Кодовое слово"); 
     if(pass == 'qq123') {
         alert('Ладно, заходи');
         document.getElementById("password-page").removeAttribute("style");
-    } else if (pass == 'pidoras' || pass == 'пидорас' || pass == 'лох' || pass == 'loh') {
+    } else if (pass == 'pidor' || pass == 'пидор' || pass == 'лох' || pass == 'loh') {
         alert('Кодовое слово, пожалуйста. Ваше имя мне не интересно');
         location.reload();
     }
@@ -12,5 +13,5 @@ function displayOpa() {
         alert('Уходи, НЕЛЬОХА!');
         location.reload();
     }
-    }
+}
 window.onload = displayOpa; 
